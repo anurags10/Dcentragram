0f5d22865b442233289bc58c8be9a89cbca0f7f9
Anurag <iamanuragsingh10@gmail.com>
2022-03-23 01:39:58 +0530

    second commit

---
+++ build/precache-manifest.6daa57901bf195ca6b5c4429139b1ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4d8512c5f4e04fa6460",
+    "url": "/static/css/main.6ae4f430.chunk.css"
+  },
+  {
+    "revision": "e4d8512c5f4e04fa6460",
+    "url": "/static/js/main.e4d8512c.chunk.js"
+  },
+  {
+    "revision": "bb6038c1ef4eb860cd42",
+    "url": "/static/css/1.05526ed4.chunk.css"
+  },
+  {
+    "revision": "bb6038c1ef4eb860cd42",
+    "url": "/static/js/1.bb6038c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "feb77700decfab3750628aba1858286f",
+    "url": "/index.html"
+  }
+];
